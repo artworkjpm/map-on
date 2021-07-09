0309b50c6268242c5eb557d2201ef677d4db172f
JohnMoran <johnnymoran44@hotmail.com>
2021-07-09 10:49:40 +0200

    Window component set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.scss";
+import Window from "./components/Window/Window";
 
 function App() {
 	return (
 		<div className="main-wrapper">
 			<img src="/images/mapon-colour.svg" alt="" />
-			<div className="window"></div>
+			<div className="window">
+				<Window />
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Window/Window.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./window.scss";
+
+export default function Window() {
+	return (
+		<div className="window-wrapper">
+			<h1>Route Report</h1>
+		</div>
+	);
+}

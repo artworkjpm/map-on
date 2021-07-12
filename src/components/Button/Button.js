c32e0ca344090470d2d1a1b070fca228f9c435cb
JohnMoran <johnnymoran44@hotmail.com>
2021-07-12 18:59:17 +0200

    styled button bottom

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function Button({ handleClick, disabled, title }) {
 	return (
-		<div>
-			<button onClick={handleClick} disabled={disabled}>
-				{title}
-			</button>
-		</div>
+		<button onClick={handleClick} disabled={disabled}>
+			{title}
+		</button>
 	);
 }
